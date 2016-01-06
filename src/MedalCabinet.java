1238dcc13a520c0ee9f69d3110feb74610cddc72
Simon <s.olofsson@gmail.com>
2016-01-06 23:01:48 +0100

    Added MedalCabinet class.

---
+++ src/MedalCabinet.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class MedalCabinet {
+
+	private HashMap<String, Integer> medals = new HashMap<>();
+	
+	public HashMap<String, Integer> getMedals() {
+		
+		return medals;
+		
+	}
+}
